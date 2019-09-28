3347e5067f546838f5d5e206fc44a821a0038f26
Emil <emil_hockey@mail.ru>
2019-09-28 13:27:38 +0300

    Second Commit

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.test2;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,18 +11,22 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
+    ImageView imageView, imageView1;
     Animation animOne;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView3);
-
+        imageView1 = (ImageView)findViewById(R.id.imageView);
+        imageView1.setBackgroundResource(R.drawable.wav);
+        final AnimationDrawable animationDrawable = (AnimationDrawable)imageView1.getBackground();
         animOne = AnimationUtils.loadAnimation(this, R.anim.transl);
+        animationDrawable.start();
+        imageView.startAnimation(animOne);
 
     }
-    public void onAnimationImageOne(View v){
-        imageView.startAnimation(animOne);
-    }
+//    public void onAnimationImageOne(View v){
+//        imageView.startAnimation(animOne);
+//    }
 }
